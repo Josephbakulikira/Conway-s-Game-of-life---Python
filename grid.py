6c5a69907a4cfb1a6562e8d9fbe1b106353505da
Joseph <48150537+Josephbakulikira@users.noreply.github.com>
2021-07-17 17:57:15 +0530

    add events , handle mouse

--- grid.py
+++ grid.py
@@ -12,13 +12,14 @@ class Grid:
         self.size = (self.rows, self.columns)
         self.grid_array = np.ndarray(shape=(self.size))
         self.offset = offset
+
     def random2d_array(self):
         for x in range(self.rows):
             for y in range(self.columns):
                 self.grid_array[x][y] = random.randint(0,1)
 
 
-    def Conway(self, off_color, on_color, surface):
+    def Conway(self, off_color, on_color, surface, pause):
         for x in range(self.rows):
             for y in range(self.columns):
                 y_pos = y * self.scale
@@ -30,17 +31,26 @@ class Grid:
                     pygame.draw.rect(surface, off_color, [x_pos, y_pos, self.scale-self.offset, self.scale-self.offset])
 
         next = np.ndarray(shape=(self.size))
-        for x in range(self.rows):
-            for y in range(self.columns):
-                state = self.grid_array[x][y]
-                neighbours = self.get_neighbours( x, y)
-                if state == 0 and neighbours == 3:
-                    next[x][y] = 1
-                elif state == 1 and (neighbours < 2 or neighbours > 3):
-                    next[x][y] = 0
-                else:
-                    next[x][y] = state
-        self.grid_array = next
+        if pause == False:
+            for x in range(self.rows):
+                for y in range(self.columns):
+                    state = self.grid_array[x][y]
+                    neighbours = self.get_neighbours( x, y)
+                    if state == 0 and neighbours == 3:
+                        next[x][y] = 1
+                    elif state == 1 and (neighbours < 2 or neighbours > 3):
+                        next[x][y] = 0
+                    else:
+                        next[x][y] = state
+            self.grid_array = next
+
+    def HandleMouse(self, x, y):
+        _x = x//self.scale
+        _y = y//self.scale
+
+        if self.grid_array[_x][_y] != None:
+            self.grid_array[_x][_y] = 1
+        
 
     def get_neighbours(self, x, y):
         total = 0
--- main.py
+++ main.py
@@ -7,27 +7,27 @@ import grid
 
 os.environ["SDL_VIDEO_CENTERED"]='1'
 
-width, height = 1920,1080
+width, height = 1000,1000
 size = (width, height)
 
 pygame.init()
 pygame.display.set_caption("CONWAY'S GAME OF LIFE")
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
-fps = 60
+fps = 30
 
 black = (0, 0, 0)
 blue = (0, 121, 150)
 blue1 = (0,14,71)
 white = (255, 255, 255)
 
-scaler = 40
+scaler = 30
 offset = 1
 
 Grid = grid.Grid(width,height, scaler, offset)
 Grid.random2d_array()
 
-
+pause = False
 run = True
 while run:
     clock.tick(fps)
@@ -36,8 +36,19 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_ESCAPE:
+                run = False
+            if event.key == pygame.K_SPACE:
+                pause = not pause
+    
+    Grid.Conway(off_color=white, on_color=blue1, surface=screen, pause=pause)
 
-    Grid.Conway(off_color=white, on_color=blue1, surface=screen)
+    if pygame.mouse.get_pressed()[0]:
+        mouseX, mouseY = pygame.mouse.get_pos()
+        Grid.HandleMouse(mouseX, mouseY)
+
+
     pygame.display.update()
 
 pygame.quit()
